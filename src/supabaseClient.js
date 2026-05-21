fe479d1160f00a10bddf9fecdd784573e5d5f710
francojbe <francojbe@gmail.com>
2026-05-20 23:30:19 -0400

    Ocultar credenciales de Supabase en variables de entorno

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://nncjrgfeoynznmmpcuni.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5uY2pyZ2Zlb3luem5tbXBjdW5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2NjQ4NTksImV4cCI6MjA4NDI0MDg1OX0.PqwQJtwL_pk7k58hUm2hiDRWo_9Z45UUwknsp64u9Dw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
